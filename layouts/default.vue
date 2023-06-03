ec7e911ed8566e3936d86a19093cfad9145ee579
Furkan <furkan.selim1999@hotmail.com>
2023-06-03 16:33:08 +0200

    full upgrade to nuxt3

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,118 +1,108 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+  <div>
+    <header>
+      <div className="wrapper">
+        <nav>
+          <div className="nav-connected">
+            <NuxtLink to="/"><img className="logo" src="../assets/logo-removebg.png" alt="logo"></NuxtLink>
+            <NuxtLink class="link" to="/"><p className="welcome-text">MensaMania</p></NuxtLink>
+            <font-awesome-icon :icon="['fasl', 'gear']" size="2xs"/>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <footer>
+      <div className="wrapper">
+        <nav>
+          <div className="nav-connected">
+            <NuxtLink class="link" to="/x1">Nav1</NuxtLink>
+            <NuxtLink class="link" to="/">Home</NuxtLink>
+            <NuxtLink class="link" to="/x1">Nav2</NuxtLink>
+          </div>
+        </nav>
+      </div>
+    </footer>
+    <Nuxt/>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'DefaultLayout',
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+  data() {
+    return {}
   }
 }
 </script>
+
+<style>
+nav {
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-template-rows: 1fr;
+  background-color: #FFA500;
+  align-items: center;
+  justify-content: center;
+}
+
+.nav-connected {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.link {
+  text-decoration: none;
+  font-size: 24px;
+  color: #333333;
+  transition: 0.4s;
+  padding: 5px;
+  box-sizing: border-box;
+}
+
+.logo {
+  margin-right: 5px;
+}
+
+.link:hover, .link:active, .link:focus {
+  color: #FFA500;
+  background-color: #333333;
+  border-radius: 5px;
+  padding: 5px;
+}
+
+.logo {
+  max-width: 75px;
+}
+
+.welcome-text {
+  font-size: 26px;
+  font-family: Pacifico;
+  font-weight: 300;
+  text-align: center;
+}
+
+.welcome-text:hover {
+  cursor: default;
+}
+
+footer {
+  background-color: #FFA500;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+footer .wrapper {
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+}
+
+footer nav {
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-template-rows: 1fr;
+  align-items: center;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+    ssr: false,
 
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+    ],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        '@vite-pwa/nuxt'
+    ],
+
+    // PWA module configuration: https://go.nuxtjs.dev/pwa
+    pwa: {
+        manifest: {
+            lang: 'en'
+        }
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    },
+    devServerHandlers: [],
 })
