f77f96b922a4db38a1fc0569a24a14acc0f2f1bc
leonkorth <leonkorth@web.de>
2023-06-19 20:51:42 +0200

    make constants private

--- static/RestClient.ts
+++ static/RestClient.ts
@@ -1,11 +1,11 @@
 
 export class RestClient {
 
-    static BASE_URL: string ="https://mensa.projekt-ipa.tech/api/v1/"
-    static ENDPOINT_CANTEEN: string = "canteen"
-    static ENDPOINT_MEAL: string = "meal"
-    static ENDPOINT_MENUE: string = "menue"
-    static API_KEY: string = "j9z44OyH2kfIO8g26AaR4y3iOAThP+3qxo56A5290C0VCXPhYKjbvpE21pGD+Y0KI8s66yrdEZQQ/HZFnO5ijLg06ORBRJWsAuCg/9RuewOTwvmw8cdloEVBlIg5gdkSWnILeJc8a9FNHBc+fY93K1DpXEDdmcjnn1zsl7yoN989Q+cWZTgg9x4kbIY3wF+VEyTpuObzNFGUK3S5cn04KGogb4GEGQDGmSZ0gpTOKxQH8PBBUJ+DXaSCv1lWhYhH1Rxb7e+8Cxm84ohPQQcsuJFPVyWo/XuETUpRkP/bK/JPyJC0CdKwc4TreArj/ilqNKszJwfc4D9oDaEWPPHweQ=="
+    private static BASE_URL: string ="https://mensa.projekt-ipa.tech/api/v1/"
+    private static ENDPOINT_CANTEEN: string = "canteen"
+    private static ENDPOINT_MEAL: string = "meal"
+    private static ENDPOINT_MENUE: string = "menue"
+    private static API_KEY: string = "j9z44OyH2kfIO8g26AaR4y3iOAThP+3qxo56A5290C0VCXPhYKjbvpE21pGD+Y0KI8s66yrdEZQQ/HZFnO5ijLg06ORBRJWsAuCg/9RuewOTwvmw8cdloEVBlIg5gdkSWnILeJc8a9FNHBc+fY93K1DpXEDdmcjnn1zsl7yoN989Q+cWZTgg9x4kbIY3wF+VEyTpuObzNFGUK3S5cn04KGogb4GEGQDGmSZ0gpTOKxQH8PBBUJ+DXaSCv1lWhYhH1Rxb7e+8Cxm84ohPQQcsuJFPVyWo/XuETUpRkP/bK/JPyJC0CdKwc4TreArj/ilqNKszJwfc4D9oDaEWPPHweQ=="
 
     private static getHeaders() {
         const headers = new Headers();
