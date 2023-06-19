0c07cd5d3568f8b9dd3d4ee2c479dc70a9d863ff
NickMendel <nick.mendel@student.htw-berlin.de>
2023-06-19 20:07:30 +0200

    fix middleware - mistake in name

--- middleware/isVisited.global.ts
+++ middleware/isVisited.global.ts
@@ -3,7 +3,7 @@ import { defineNuxtRouteMiddleware, navigateTo} from '#app'
 export default defineNuxtRouteMiddleware((to) => {
     console.log('to: ', to);
     console.log('Visited: ', localStorage.getItem('hasVisited'));
-    console.log('Favorite Canteen: ', localStorage.getItem('favoriteCanteenName'));
+    console.log('Favorite Canteen: ', localStorage.getItem('favoriteCanteen'));
     console.log('User Role: ', localStorage.getItem('userRole'));
     const hasVisited = localStorage.getItem('hasVisited');
 
