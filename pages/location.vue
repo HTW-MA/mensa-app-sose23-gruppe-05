d7e0281065e1ede7d19b7ad6153e3572e5c56dd2
Furkan <furkan.selim1999@hotmail.com>
2023-06-26 18:07:24 +0200

    - added functionality to track users location with button click

--- pages/location.vue
+++ pages/location.vue
@@ -2,21 +2,53 @@
   <div>
     <h3>Location</h3>
     <div id="map" style="height: 400px;"></div>
+    <button @click="trackLocation">Share Location</button>
   </div>
-  <h2>Work is still in progress</h2>
 </template>
 
 <script setup lang="ts">
-import L, { LatLngExpression } from 'leaflet';
+import L, { LatLngExpression, Marker, LatLngBounds } from 'leaflet';
+import { ref } from 'vue';
 
 const latitude = 51.505;
 const longitude = -0.09;
 const zoomLevel = 13;
 
-const markerLat = 51.505; //
-const markerLng = -0.09; //
+const markerLat = 51.505;
+const markerLng = -0.09;
 
 let map: L.Map;
+let marker: Marker | null = null;
+const currentLocation = ref<LatLngExpression | null>(null);
+
+function addMarker(latlng: LatLngExpression) {
+  if (marker) {
+    map.removeLayer(marker);
+  }
+  marker = L.marker(latlng).addTo(map);
+}
+
+function trackLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      currentLocation.value = [latitude, longitude];
+      addMarker([latitude, longitude]);
+      fitMapBounds();
+    });
+  } else {
+    console.log('Geolocation is not supported by this browser.');
+  }
+}
+
+function fitMapBounds() {
+  const markerLatLng = marker?.getLatLng();
+  const currentLatLng = currentLocation.value;
+  if (markerLatLng && currentLatLng) {
+    const bounds = L.latLngBounds([markerLatLng, currentLatLng]);
+    map.fitBounds(bounds);
+  }
+}
 
 onMounted(() => {
   map = L.map('map').setView([latitude, longitude], zoomLevel);
@@ -24,13 +56,8 @@ onMounted(() => {
     attribution: '© OpenStreetMap contributors'
   }).addTo(map);
 
-  const markerLatLng: LatLngExpression = [markerLat, markerLng];
-  addMarker(markerLatLng);
+  addMarker([markerLat, markerLng]);
 });
-
-function addMarker(latlng: LatLngExpression) {
-  L.marker(latlng).addTo(map);
-}
 </script>
 
 <style scoped>
